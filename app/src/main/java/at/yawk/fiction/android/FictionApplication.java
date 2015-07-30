1ef36560c14befc1c2a6e06588338949da21dd49
Jonas Konrad <me@yawk.at>
2015-07-30 18:43:10 +0200

    Better logging

--- app/src/main/java/at/yawk/fiction/android/FictionApplication.java
+++ app/src/main/java/at/yawk/fiction/android/FictionApplication.java
@@ -5,10 +5,12 @@ import android.os.Environment;
 import at.yawk.fiction.android.context.FictionContext;
 import java.io.File;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author yawkat
  */
+@Slf4j
 public class FictionApplication extends Application {
     @Getter private FictionContext context;
 
@@ -16,6 +18,9 @@ public class FictionApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        log.info("Starting application");
+        log.debug("Creating context...");
         context = new FictionContext(this, new File(Environment.getExternalStorageDirectory(), "Fiction"));
+        log.debug("Context created.");
     }
 }
