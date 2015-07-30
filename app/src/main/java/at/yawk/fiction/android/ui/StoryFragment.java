9c003dd6de82553625d3cca8c3a6aa9ceb042d5b
Jonas Konrad <me@yawk.at>
2015-07-30 22:25:35 +0200

    Refactor StoryFragment for future changes

--- app/src/main/java/at/yawk/fiction/android/ui/StoryFragment.java
+++ app/src/main/java/at/yawk/fiction/android/ui/StoryFragment.java
@@ -31,6 +31,7 @@ public class StoryFragment extends Fragment implements ContextProvider {
     private StoryWrapper wrapper;
     private FictionContext fictionContext;
     private View root;
+    private ViewGroup chapterGroup;
 
     @Override
     public FictionContext getContext() {
@@ -58,6 +59,7 @@ public class StoryFragment extends Fragment implements ContextProvider {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.story, container, false);
+        chapterGroup = (ViewGroup) root.findViewById(R.id.chapters);
         root.findViewById(R.id.title).setOnClickListener(v -> getContext().getTaskManager().execute(taskContext, () -> {
             try {
                 getContext().getStorageManager().getEpubBuilder().openEpub(getActivity(), wrapper.getStory());
@@ -70,7 +72,7 @@ public class StoryFragment extends Fragment implements ContextProvider {
         return root;
     }
 
-    private List<View> chapterViews = new ArrayList<>();
+    private List<ChapterHolder> chapterHolders = new ArrayList<>();
 
     private void refresh() {
         ((TextView) root.findViewById(R.id.title)).setText(wrapper.getStory().getTitle());
@@ -89,43 +91,23 @@ public class StoryFragment extends Fragment implements ContextProvider {
             descriptionView.setText("");
         }
 
-        ViewGroup chapterGroup = (ViewGroup) root.findViewById(R.id.chapters);
 
         List<? extends Chapter> chapters = wrapper.getStory().getChapters();
         for (int i = 0; i < chapters.size(); i++) {
-            View view;
-            if (i >= chapterViews.size()) {
-                view = getActivity().getLayoutInflater().inflate(R.layout.chapter, chapterGroup, false);
+            ChapterHolder holder;
+            if (i >= chapterHolders.size()) {
+                View view = getActivity().getLayoutInflater().inflate(R.layout.chapter, chapterGroup, false);
                 chapterGroup.addView(view);
-                chapterViews.add(view);
+                chapterHolders.add(holder = new ChapterHolder(view, i));
             } else {
-                view = chapterViews.get(i);
+                holder = chapterHolders.get(i);
             }
 
-            Chapter chapter = chapters.get(i);
-            String name = chapter.getName();
-            if (name == null) {
-                name = "Chapter " + (i + 1);
-            }
-            ((TextView) view.findViewById(R.id.chapterName)).setText(name);
-
-            boolean hasText = chapter.getText() != null;
-
-            setChapterViewStatus(view, hasText ? R.id.chapterRefresh : R.id.chapterDownload);
-
-            final int finalI = i;
-            View.OnClickListener refreshListener = v -> fetchChapter(view, finalI);
-            view.findViewById(R.id.chapterDownload).setOnClickListener(refreshListener);
-            view.findViewById(R.id.chapterRefresh).setOnClickListener(refreshListener);
-
-            CheckBox readBox = (CheckBox) view.findViewById(R.id.chapterRead);
-            readBox.setVisibility(hasText ? View.VISIBLE : View.INVISIBLE);
-            readBox.setChecked(wrapper.isChapterRead(chapter));
-            readBox.setOnCheckedChangeListener((buttonView, isChecked) -> wrapper.setChapterRead(chapter, isChecked));
+            holder.setChapter(chapters.get(i));
         }
 
-        if (chapters.size() < chapterViews.size()) {
-            List<View> toRemove = chapterViews.subList(chapters.size(), chapterViews.size());
+        if (chapters.size() < chapterHolders.size()) {
+            List<ChapterHolder> toRemove = chapterHolders.subList(chapters.size(), chapterHolders.size());
             for (int i = 0; i < toRemove.size(); i++) {
                 chapterGroup.removeViewAt(chapters.size() + i);
             }
@@ -133,37 +115,74 @@ public class StoryFragment extends Fragment implements ContextProvider {
         }
     }
 
-    private void setChapterViewStatus(View chapterView, int statusId) {
-        ViewGroup group = (ViewGroup) chapterView.findViewById(R.id.downloadWrapper);
-        for (int i = 0; i < group.getChildCount(); i++) {
-            View child = group.getChildAt(i);
-            child.setVisibility(child.getId() == statusId ? View.VISIBLE : View.INVISIBLE);
-        }
-    }
-
-    private void fetchChapter(View chapterView, int index) {
-        setChapterViewStatus(chapterView, R.id.chapterDownloading);
-
-        Story storyClone = getContext().getStorageManager().getPojoMerger().clone(wrapper.getStory());
-        Chapter chapter = storyClone.getChapters().get(index);
-        AndroidFictionProvider provider = getContext().getProviderManager().getProvider(storyClone);
-        getContext().getTaskManager().execute(taskContext, () -> {
-            try {
-                provider.fetchChapter(storyClone, chapter);
-                getContext().getStorageManager().getTextStorage().externalize(chapter);
-
-                getContext().getStorageManager().mergeStory(storyClone);
-                getActivity().runOnUiThread(this::refresh);
-            } catch (Exception e) {
-                log.error("Failed to fetch chapter", e);
-                getContext().toast(getActivity(), "Failed to fetch chapter", e);
-            }
-        });
-    }
-
     @Override
     public void onDestroy() {
         super.onDestroy();
         taskContext.destroy();
+    }
+
+    private class ChapterHolder {
+        private final View view;
+        private final int index;
+        private Chapter chapter;
+
+        private final CheckBox readBox;
+
+        ChapterHolder(View view, int index) {
+            this.view = view;
+            this.index = index;
+
+            readBox = (CheckBox) view.findViewById(R.id.chapterRead);
+            readBox.setOnCheckedChangeListener((buttonView, isChecked) -> wrapper.setChapterRead(chapter, isChecked));
+
+            View.OnClickListener refreshListener = v -> fetchChapter();
+            view.findViewById(R.id.chapterDownload).setOnClickListener(refreshListener);
+            view.findViewById(R.id.chapterRefresh).setOnClickListener(refreshListener);
+        }
+
+        void setChapter(Chapter chapter) {
+            this.chapter = chapter;
+
+            String name = chapter.getName();
+            if (name == null) {
+                name = "Chapter " + (index + 1);
+            }
+            ((TextView) view.findViewById(R.id.chapterName)).setText(name);
+
+            boolean hasText = chapter.getText() != null;
+
+            setChapterViewStatus(hasText ? R.id.chapterRefresh : R.id.chapterDownload);
+
+            readBox.setVisibility(hasText ? View.VISIBLE : View.INVISIBLE);
+            readBox.setChecked(wrapper.isChapterRead(chapter));
+        }
+
+        void fetchChapter() {
+            setChapterViewStatus(R.id.chapterDownloading);
+
+            Story storyClone = getContext().getStorageManager().getPojoMerger().clone(wrapper.getStory());
+            Chapter chapter = storyClone.getChapters().get(index);
+            AndroidFictionProvider provider = getContext().getProviderManager().getProvider(storyClone);
+            getContext().getTaskManager().execute(taskContext, () -> {
+                try {
+                    provider.fetchChapter(storyClone, chapter);
+                    getContext().getStorageManager().getTextStorage().externalize(chapter);
+
+                    getContext().getStorageManager().mergeStory(storyClone);
+                    getActivity().runOnUiThread(StoryFragment.this::refresh);
+                } catch (Exception e) {
+                    log.error("Failed to fetch chapter", e);
+                    getContext().toast(getActivity(), "Failed to fetch chapter", e);
+                }
+            });
+        }
+
+        private void setChapterViewStatus(int statusId) {
+            ViewGroup group = (ViewGroup) view.findViewById(R.id.downloadWrapper);
+            for (int i = 0; i < group.getChildCount(); i++) {
+                View child = group.getChildAt(i);
+                child.setVisibility(child.getId() == statusId ? View.VISIBLE : View.INVISIBLE);
+            }
+        }
     }
 }
