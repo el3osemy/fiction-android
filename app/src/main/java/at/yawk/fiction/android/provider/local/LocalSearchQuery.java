7aa96080d97dd0b5bb9f537d1c57306dcf6a4a70
Jonas Konrad <me@yawk.at>
2015-08-01 17:21:28 +0200

    Another take at fixing downloaded filter

--- app/src/main/java/at/yawk/fiction/android/provider/local/LocalSearchQuery.java
+++ app/src/main/java/at/yawk/fiction/android/provider/local/LocalSearchQuery.java
@@ -29,7 +29,7 @@ public class LocalSearchQuery extends SearchQuery {
         int chapterCount = wrapper.getStory().getChapters().size();
         int downloadedCount = 0;
         for (int i = 0; i < chapterCount; i++) {
-            if (wrapper.isChapterRead(i)) {
+            if (wrapper.hasChapterText(i)) {
                 downloadedCount++;
             }
         }
