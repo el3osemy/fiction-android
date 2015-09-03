55cc162d50c857459f014f8902019779dd1cb70b
Jonas Konrad <me@yawk.at>
2015-09-03 12:00:37 +0200

    Use fake firefox user agent

--- app/src/main/java/at/yawk/fiction/android/provider/HttpClientFactory.java
+++ app/src/main/java/at/yawk/fiction/android/provider/HttpClientFactory.java
@@ -1,10 +1,12 @@
 package at.yawk.fiction.android.provider;
 
+import java.util.Collections;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import lib.org.apache.http.client.HttpClient;
 import lib.org.apache.http.impl.client.HttpClientBuilder;
 import lib.org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import lib.org.apache.http.message.BasicHeader;
 
 /**
  * @author yawkat
@@ -22,6 +24,8 @@ public class HttpClientFactory {
     public HttpClient createHttpClient() {
         return HttpClientBuilder.create()
                 .setConnectionManager(connectionManager)
+                .setDefaultHeaders(Collections.singleton(new BasicHeader(
+                        "User-Agent", "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0")))
                 .build();
     }
 }
