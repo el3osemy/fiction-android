81359e998913c0ba4f6fd04a949057e650dc8ee7
Jonas Konrad <me@yawk.at>
2015-09-12 23:18:10 +0200

    Add a tag editor to the fim query editor

--- app/src/main/java/at/yawk/fiction/android/provider/fim/FimQueryEditorFragment.java
+++ app/src/main/java/at/yawk/fiction/android/provider/fim/FimQueryEditorFragment.java
@@ -1,8 +1,12 @@
 package at.yawk.fiction.android.provider.fim;
 
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import at.yawk.fiction.android.R;
 import at.yawk.fiction.android.context.FragmentUiRunner;
 import at.yawk.fiction.android.context.TaskContext;
@@ -13,9 +17,13 @@ import at.yawk.fiction.android.inject.SupportFragmentModule;
 import at.yawk.fiction.android.ui.QueryEditorFragment;
 import at.yawk.fiction.impl.fimfiction.*;
 import butterknife.Bind;
+import com.google.common.collect.Lists;
 import dagger.Module;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 
@@ -32,6 +40,9 @@ public class FimQueryEditorFragment extends QueryEditorFragment<FimSearchQuery> 
     @Inject Toasts toasts;
     @Inject FragmentUiRunner uiRunner;
 
+    @Nullable
+    private List<FimTag> tags = null;
+
     @Bind(R.id.order) Spinner order;
     @Bind(R.id.publishTime) Spinner publishTime;
     @Bind(R.id.status) Spinner status;
@@ -39,6 +50,11 @@ public class FimQueryEditorFragment extends QueryEditorFragment<FimSearchQuery> 
     @Bind(R.id.maxWords) EditText maxWords;
     @Bind(R.id.shelf) Spinner shelf;
     @Bind(R.id.unread) CheckBox unread;
+    @Bind(R.id.tagList) ViewGroup tagList;
+    @Bind(R.id.addTag) View addTag;
+
+    private Set<FimTag> includedTags;
+    private Set<FimTag> excludedTags;
 
     @Override
     protected void bind() {
@@ -83,6 +99,88 @@ public class FimQueryEditorFragment extends QueryEditorFragment<FimSearchQuery> 
 
             uiRunner.runOnUiThread(() -> bindShelves(shelves));
         });
+
+        includedTags = new HashSet<>();
+        if (getQuery().getIncludedTags() != null) { includedTags.addAll(getQuery().getIncludedTags()); }
+        excludedTags = new HashSet<>();
+        if (getQuery().getExcludedTags() != null) { excludedTags.addAll(getQuery().getExcludedTags()); }
+        addTag.setOnClickListener(v -> openTagAdder());
+        updateTags();
+    }
+
+    private void openTagAdder() {
+        if (tags == null) {
+            taskManager.execute(taskContext, () -> {
+                try {
+                    tags = fimAndroidFictionProvider.getFictionProvider().fetchTags();
+                } catch (Exception e) {
+                    log.error("Failed to fetch tags", e);
+                    toasts.toast("Failed to fetch tags", e);
+                }
+                uiRunner.runOnUiThread(this::openTagAdder);
+            });
+            return;
+        }
+        if (tags.isEmpty()) {
+            toasts.toast("No tags found");
+            return;
+        }
+
+        Set<FimTag> selected = new HashSet<>();
+        String[] tagNames = Lists.transform(tags, FimTag::getName).toArray(new String[tags.size()]);
+
+        new AlertDialog.Builder(getActivity())
+                .setMultiChoiceItems(tagNames, new boolean[tags.size()], (dialog, which, isChecked) -> {
+                    if (isChecked) {
+                        selected.add(tags.get(which));
+                    } else {
+                        selected.remove(tags.get(which));
+                    }
+                })
+                .setTitle(R.string.filter_tag)
+                .setPositiveButton(R.string.include, (dg, which) -> {
+                    excludedTags.removeAll(selected);
+                    includedTags.addAll(selected);
+                    updateTags();
+                })
+                .setNegativeButton(R.string.exclude, (dg, which) -> {
+                    includedTags.removeAll(selected);
+                    excludedTags.addAll(selected);
+                    updateTags();
+                })
+                .setNeutralButton(R.string.cancel, (dg, which) -> {
+                    dg.dismiss();
+                })
+                .show();
+    }
+
+    private void updateTags() {
+        // remove all children except the addTag button
+        tagList.removeViews(0, tagList.getChildCount() - 1);
+
+        for (FimTag included : includedTags) {
+            TextView view = (TextView) getActivity().getLayoutInflater()
+                    .inflate(R.layout.query_editor_fim_tag, tagList, false);
+            view.setBackgroundColor(0xff1b5e20);
+            view.setText(included.getName());
+            view.setOnClickListener(v -> {
+                includedTags.remove(included);
+                updateTags();
+            });
+            tagList.addView(view, tagList.getChildCount() - 1);
+        }
+
+        for (FimTag excluded : excludedTags) {
+            TextView view = (TextView) getActivity().getLayoutInflater()
+                    .inflate(R.layout.query_editor_fim_tag, tagList, false);
+            view.setBackgroundColor(0xffb71c1c);
+            view.setText(excluded.getName());
+            view.setOnClickListener(v -> {
+                excludedTags.remove(excluded);
+                updateTags();
+            });
+            tagList.addView(view, tagList.getChildCount() - 1);
+        }
     }
 
     private void bindShelves(List<FimShelf> shelves) {
@@ -91,6 +189,13 @@ public class FimQueryEditorFragment extends QueryEditorFragment<FimSearchQuery> 
                    FimSearchQuery::setShelf,
                    s -> s == null ? "Any Shelf" : s.getName(),
                    andNull(shelves));
+    }
+
+    @Override
+    public void save() {
+        super.save();
+        getQuery().setExcludedTags(excludedTags);
+        getQuery().setIncludedTags(includedTags);
     }
 
     @Override
